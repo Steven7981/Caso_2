5ea47cb2a48f996b9bfe521f1e1da721fbd3933c
Isaac Alfaro <103698346+NightmareG113@users.noreply.github.com>
2023-09-17 21:24:12 -0600

    .hs con informacion del caso

---
+++ camionComida.h
@@ -0,0 +1,5 @@
+#pragma once
+/*COLA
+se crea un camion de comida, que consiste en una cola de personas que salen de las graderias
+y estan en una cola para ser atendidos en este camion de comida, solo tiene el nombre del camion,
+cupo maximo de personas en cola y dicha cola para atender a las personas.*/
---
+++ persona.h
@@ -0,0 +1,7 @@
+#pragma once
+/*se crean los diferentes tipos de clases de personas que hay
+sea, 
+fanatico con su bool entrada
+cantante con nombre, cancion, y la COLA de canciones
+seguridadCantante quienes son los que cuidan el escenario 
+seguridad zona, que son los que se encargan de revisar entradas.*/
