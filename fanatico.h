31319e4f763f6209725a4e599654d4d484531269
Steven7981 <stevenfengwu79@gmail.com>
2023-09-28 23:13:02 -0600

    Create fanatico.h

---
+++ fanatico.h
@@ -0,0 +1,13 @@
+#include "persona.h"
+#include <iostream>
+#include <string>
+
+class Fanatico{
+    private:
+        std::string nombre;
+    
+    public:
+        void fanatico(Persona persona){
+            //crea un fan
+        }
+};
