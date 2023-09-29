3848d34ac75d12595c15c25bc4d2b791dab56f16
Steven7981 <stevenfengwu79@gmail.com>
2023-09-28 23:36:50 -0600

    Update filaComida.h

--- filaComida.h
+++ filaComida.h
@@ -12,4 +12,7 @@ class filaComida{
         void agregarFila(std::queue<Fanatico> colaComida){
             //agrega a la cola de personas que esta haciendo fila para comida
         }
+        std::queue<Fanatico> getFilaComida(){
+            //accesa a la fila de comida y lo retorna
+        }
 };
