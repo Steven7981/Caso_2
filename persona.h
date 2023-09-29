441f98f2dae535d973a95d224c91c713d50986f1
Steven7981 <stevenfengwu79@gmail.com>
2023-09-28 22:51:05 -0600

    Update persona.h

--- persona.h
+++ persona.h
@@ -1,7 +1,10 @@
-#pragma once
-/*se crean los diferentes tipos de clases de personas que hay
-sea, 
-fanatico con su bool entrada
-cantante con nombre, cancion, y la COLA de canciones
-seguridadCantante quienes son los que cuidan el escenario 
-seguridad zona, que son los que se encargan de revisar entradas.*/
+#include <iostream>
+class Persona{
+    private:
+        std::string tipo;
+
+    public:
+        std::string persona(std::string tipo){
+            return "Persona tipo" + tipo; //constructor
+        }
+};
