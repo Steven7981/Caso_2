9532c6858d32a454bb26569c9496075f1de4ab08
Steven7981 <stevenfengwu79@gmail.com>
2023-09-28 23:36:48 -0600

    Update camionComida.h

--- camionComida.h
+++ camionComida.h
@@ -1,5 +1,9 @@
-#pragma once
-/*COLA
-se crea un camion de comida, que consiste en una cola de personas que salen de las graderias
-y estan en una cola para ser atendidos en este camion de comida, solo tiene el nombre del camion,
-cupo maximo de personas en cola y dicha cola para atender a las personas.*/
+#include "filaComida.h"
+#include <iostream>
+
+class Comida{
+    public:
+        void atender(std::queue<Fanatico> ){
+            //
+        }
+};
