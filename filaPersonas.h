b87843638d60cdab278e6a3662e24f87d5fbf916
Steven7981 <stevenfengwu79@gmail.com>
2023-09-28 23:13:06 -0600

    Update filaPersonas.h

--- filaPersonas.h
+++ filaPersonas.h
@@ -1,17 +1,17 @@
-#include "persona.h"
+#include "fanatico.h"
 #include <queue>
 #include <iostream>
 #include <string>
 
 class filaPersona{
     private:
-        std::queue<Persona> fila;
+        std::queue<Fanatico> fila;
     public:
-        void agregarPersonas(Persona persona){
-            fila.push(persona);
+        void agregarPersonas(Fanatico fan){
+            fila.push(fan);
         }
 
-        std::queue<Persona> getFila(){
+        std::queue<Fanatico> getFila(){
             return fila;
         }
 };
