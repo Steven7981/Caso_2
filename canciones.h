590b0a304a2940393becc705ea9460ef661fc071
Steven7981 <stevenfengwu79@gmail.com>
2023-09-29 09:43:15 -0600

    Create canciones.h

---
+++ canciones.h
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Cancion{
+    private:
+        int duracion;
+    
+    public:
+        void cancion(int duracion){
+            //constructor de  cancion
+        }
+
+};
