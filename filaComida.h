3fc34e20a967accd513f45dda0e4605d3b40fc88
Steven7981 <stevenfengwu79@gmail.com>
2023-09-28 23:31:54 -0600

    Create filaComida.h

---
+++ filaComida.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "fanatico.h"
+#include <queue>
+#include "filaPersonas.h"
+
+class filaComida{
+    private:
+        std::queue<Fanatico> colaComida;
+
+    public:
+        void agregarFila(std::queue<Fanatico> colaComida){
+            //agrega a la cola de personas que esta haciendo fila para comida
+        }
+};
