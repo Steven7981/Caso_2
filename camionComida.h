0d3410380d85935069dc272b5e7ef9396c9daa0a
Isaac Alfaro <103698346+NightmareG113@users.noreply.github.com>
2023-09-29 17:31:58 -0600

    Update camionComida.h

--- camionComida.h
+++ camionComida.h
@@ -1,9 +1,10 @@
 #include "filaComida.h"
 #include <iostream>
+using namespace std;
 
 class Comida{
     public:
-        void atender(std::queue<Fanatico> ){
-            //recibe la cola de colaComia y agarra a uno de la cola para ir atendiendo
+        void atender(queue<Fanatico> colaComida ){
+            //recibe la cola de colaComida y agarra a uno de la cola para ir atendiendo
         }
 };
