0c0b1225ba216a28c9f9965c1143be13a6582ba3
Steven7981 <stevenfengwu79@gmail.com>
2023-09-29 09:31:09 -0600

    Update camionComida.h

--- camionComida.h
+++ camionComida.h
@@ -4,6 +4,6 @@
 class Comida{
     public:
         void atender(std::queue<Fanatico> ){
-            //
+            //recibe la cola de colaComia y agarra a uno de la cola para ir atendiendo
         }
 };
