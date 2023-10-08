756f215a01f0183456d1bb28cf95ba7b42dd38be
Steven7981 <stevenfengwu79@gmail.com>
2023-10-08 00:02:30 -0600

    Update filaComida.h

--- filaComida.h
+++ filaComida.h
@@ -4,15 +4,34 @@
 #include <queue>
 #include "filaPersonas.h"
 
+using namespace std;
+
+#include "json.hpp"
+#include <fstream>
+
+using json = nlohmann::json;
+
+string nombreArchivo = "caso.json";
+ifstream archivo(nombreArchivo);
+
+json datos;
+archivo >> datos;
+int personasEnFilaComidaMax = datos["cantidad de personas en fila para comida"]["maximo"];
+
+// cola de personas que esta haciendo fila para camion de comida
 class filaComida{
     private:
         std::queue<Fanatico> colaComida;
 
     public:
-        void agregarFila(std::queue<Fanatico> colaComida){
-            //agrega a la cola de personas que esta haciendo fila para comida
+        void agregarFila(queue<Fanatico> fila){
+            if (!fila.size()=personasEnFilaComidaMax){
+                Fanatico persona = fila.front();
+                fila.pop();
+                colaComida.push(persona);
+            }
         }
         std::queue<Fanatico> getFilaComida(){
-            //accesa a la fila de comida y lo retorna
+            return colaComida;
         }
 };
