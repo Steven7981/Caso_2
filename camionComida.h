85b8540050d56976361ebf882234ff00230aa691
Steven7981 <stevenfengwu79@gmail.com>
2023-10-08 00:02:33 -0600

    Update camionComida.h

--- camionComida.h
+++ camionComida.h
@@ -1,9 +1,31 @@
 #include "filaComida.h"
 #include <iostream>
+#include "json.hpp"
+#include <fstream>
+#include <random>
+using namespace std;
+
+using json = nlohmann::json;
+
+string nombreArchivo = "caso.json";
+
+ifstream archivo(nombreArchivo);
+
+json datos;
+archivo >> datos;
+
+int duracionAtencionCamionMin = datos["duracion en atender en camion comida"]["minimo"];
+int duracionAtencionCamionMax = datos["duracion en atender en camion comida"]["maximo"];
 
 class Comida{
     public:
-        void atender(std::queue<Fanatico> ){
-            //recibe la cola de colaComia y agarra a uno de la cola para ir atendiendo
-        }
+        void atender(queue<Fanatico> ){
+            random_device rd;
+            mt19937 generador(rd()); 
+            uniform_int_distribution<int> distribucion(duracionAtencionCamionMin, duracionAtencionCamionMax);
+
+            //se elige un numero aleatorio entre la duracion minima y maxima, y eso es lo que durara en atender 
+            int numeroAleatorio = distribucion(generador);
+
+                }
 };
