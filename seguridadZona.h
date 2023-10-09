534f82cccbd23be36e333e83a7910d0dbea6724f
Isaac Alfaro <103698346+NightmareG113@users.noreply.github.com>
2023-10-08 21:43:54 -0600

    Add files via upload
    cambios seguridad, fan y json

--- seguridadZona.h
+++ seguridadZona.h
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <queue>
 #include "fanatico.h"
+
+
 class seguridadZona{
 private:
 	queue<Fanatico> colaFan;
 
 public:
-	void seguridadZona(){
+	seguridadZona crearSeguridad(){
 		seguridadZona Personal;
-
+		return Personal;
 	}
+
 	void revisarEntrada(Fanatico fan){
-		if fan.getEntrada() {
-			colaFan.push();
+		if (fan.getEntrada){
+			colaFan.push(fan)//cola con las personas aceptadas que pasan a la graderia
 		}
 		else {
 			cout << "El fanatico no tiene entrada." << endl;
