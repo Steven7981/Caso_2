3355370589ff1ac03f0d4b552f9e4fb0c04c6909
Steven7981 <stevenfengwu79@gmail.com>
2023-10-20 01:07:33 -0600

    Create generadorRandom.h

---
+++ generadorRandom.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <random>
+
+class RandomNumberGenerator {
+public:
+    RandomNumberGenerator(int min, int max) : distribution(min, max), generator(std::random_device()()) {}
+
+    int generateRandomNumber() {
+        return distribution(generator);
+    }
+
+private:
+    std::uniform_int_distribution<int> distribution;
+    std::mt19937 generator;
+};
