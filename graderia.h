299e79afdf99aa972bd001395afef979bc6fd7d8
Steven7981 <stevenfengwu79@gmail.com>
2023-10-20 01:07:43 -0600

    Update graderia.h

--- graderia.h
+++ graderia.h
@@ -1,36 +1,16 @@
-#include "filaPersonas.h"
 #include <stack>
 #include <iostream>
-#include "json.hpp"
 #include <fstream>
 using namespace std;
 
-using json = nlohmann::json;
-
-string nombreArchivo = "caso.json";
-
-ifstream archivo(nombreArchivo);
-
-json datos;
-archivo >> datos;
-
-int maxPersonasGraderia = datos["maxima cantidad de personas en graderia"]["maximo"];
 class Graderia{
     private:
         stack<Fanatico> colaFan;
 
     public:
-        void graderia(){
-            pilaFan = stack<Fanatico>();
-        }
-        void agregarFanatico(Fanatico fan){
-            if (pilaFan.size()<=grande){
-                if (!pilaFan.size()=maxPersonasGraderia){
-                    pilaFan.push(fan);
-                }
-            }
-        }
+    //cuando termie el concierto, ir sacando a la gente de las graderias
         void salirGraderia(){
-            pilaFan.pop();
+            colaFan.pop();
         }
+
 };
