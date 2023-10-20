1aa1e4f62cf9e4e1661185bb0fd7228db4dd7377
Steven7981 <stevenfengwu79@gmail.com>
2023-10-20 01:07:45 -0600

    Update fanatico.h

--- fanatico.h
+++ fanatico.h
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <random>
+#include "generadorRandom.h"
 using namespace std;
 
 class Fanatico {
-
+    private:
+        bool entrada = false;
     public:
         Fanatico(){
+            RandomNumberGenerator random(1,10);
+            int verificar = random.generateRandomNumber();
+            if (verificar>=5){
+                this->entrada=true;
+            }
+        }
 
+        bool mostrarEntrada(){
+            return entrada;
+        }
+        void moverFan(){
+            //ir ya sea a hacer fila al camion de comida or ir a la graderia
         }
 };
