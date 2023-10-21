0b5451286cacbc3a5e9e8a7ac05ed5c723924f87
Steven7981 <stevenfengwu79@gmail.com>
2023-10-21 01:02:46 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "simulacion.h"
+#include "cantante.h"
+#include "threads.h"
 
 using namespace std;
 
 int main(void){
     
-
+    Threads thread;
+    Cantante c1;
+   // c1.cantar();
+    thread.llegarFans();
 
 }
