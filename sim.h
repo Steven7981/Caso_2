53e107e105713dc91fa8f4fbcf732927a82afe0d
Steven7981 <stevenfengwu79@gmail.com>
2023-10-22 00:34:15 -0600

    Create sim.h

---
+++ sim.h
@@ -0,0 +1,16 @@
+//---------------colas, pilas y problemas--------------
+/*
+Colas:
+    - fila[]
+        en este se agregan los fanaticos que van llegando al concierto. Esta fila consiste
+        en la que pasa por la seguridad zona para verificar si tienen entrada o no
+
+    - colaComida[]
+        en este se agregan los fanaticos que pasaron la seguridad zona y quieren ir al camion de comida
+
+    - colaFan[]
+        este consiste en la cola de las graderias
+    
+    - colaCanciones[]
+        este es la cola en donde se van almacenando las canciones que va cantar
+*/
