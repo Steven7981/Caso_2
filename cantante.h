027a54677fab36b6f88bf4e842ed2b1a711fb7d9
Steven7981 <stevenfengwu79@gmail.com>
2023-10-22 00:34:39 -0600

    Create cantante.h

---
+++ cantante.h
@@ -0,0 +1,42 @@
+#ifndef cantante_h
+#define cantante_h
+
+
+#include <iostream>
+#include <queue>
+#include "canciones.h"
+#include "json.hpp"
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+class Cantante{
+    private:
+        queue<Cancion> colaCanciones;
+    
+    public:
+        Cantante(){
+
+        }
+        bool cantar(){
+            ifstream file("caso.json");
+            json data;
+            file >> data;
+            std::vector<int> canciones = data["canciones"];
+
+            for (auto cancion : canciones){
+                int duracion = cancion;
+                cout<<"Cantando una cancion con una duracion de "<< duracion;
+                this_thread::sleep_for(chrono::seconds(duracion));
+                canciones.erase();
+            }
+            cout<<"Terminó de cantar todas las canciones. "<< endl;
+            return true;
+            }
+        
+
+
+};
+
+#endif
