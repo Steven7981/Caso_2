c95b03a51b57e7e534fe084dc9797f72202d698f
Steven7981 <stevenfengwu79@gmail.com>
2023-10-22 00:33:42 -0600

    Update camionComida.h

--- camionComida.h
+++ camionComida.h
@@ -1,10 +1,31 @@
-#include "filaComida.h"
 #include <iostream>
+<<<<<<< HEAD
+using namespace std;
+=======
+#include <fstream>
+#include <queue>
+#include "fanatico.h"
 using namespace std;
 
+>>>>>>> acuerdo
+
 class Comida{
+    private:
+        queue<Fanatico> filaDeComida;
     public:
+<<<<<<< HEAD
         void atender(queue<Fanatico> colaComida ){
             //recibe la cola de colaComida y agarra a uno de la cola para ir atendiendo
+=======}
+        Comida(){
+
+        }
+
+        void atender(){
+            Fanatico fan;
+            fan = filaDeComida.front();
+            filaDeComida.pop();
+            cout<<"Se atendio a la persona que estaba frente a la cola de comida"<<endl;
+>>>>>>> acuerdo
         }
 };
