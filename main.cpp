60ee6691e6ca1ef52de94144c561e36a9403a71e
Steven7981 <stevenfengwu79@gmail.com>
2023-10-22 00:33:58 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "simulacion.h"
+#include "cantante.h"
+#include "threads.h"
+
+using namespace std;
+
+int main(void){
+    
+    Threads thread;
+    Cantante c1;
+   // c1.cantar();
+    thread.llegarFans();
+
+}
