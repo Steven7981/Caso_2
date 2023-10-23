ffd9b40210bdd4166cd3221a9e366079aefa03c6
Steven7981 <stevenfengwu79@gmail.com>
2023-10-23 00:00:27 -0600

    Update threads.h

--- threads.h
+++ threads.h
@@ -12,30 +12,70 @@
 
 #include <iostream>
 #include <queue>
+#include <thread>  
+#include <vector>  
+#include <stack>
+
 using namespace std::this_thread;
 using namespace std;
 
 class Threads{
     private:
         jsonDatos simulacion;
-        queue<Fanatico> fila;
+        vector<queue<Fanatico>> filas;
 
     public:
-        void llegarFans(){
+        void iniciarSim(){
             simulacion.simulacion();
+            int duracionSim = simulacion.getDuracionSimulacion();
+            int duracionConcierto = simulacion.getDuracionEntrarConcierto();
             int cantFans = simulacion.getCantidadLlegarFan();
             int tiempo_llegar = simulacion.getTiempoLlegarFan();
             int maximo_fila = simulacion.getFilaFanMax();
+            int cantFila = simulacion.getCantFilas();
+            int cantGraderias = simulacion.getGraderias();
+            int maxGraderias = simulacion.getGraderiasMax();
 
-            for (int j = 0; j<maximo_fila; j++){
-                for (int i = 0; i < cantFans; i++){
-                    Fanatico fan;
-                    fila.push(fan);
+            seguridadZona guarda;
+
+            while (duracionSim > 0){
+                    vector<queue<Fanatico>> filaFan(cantFila);
+                    int veces = maximo_fila/cantFans;
+                    for(int j = 0; j < cantFila; j++){
+                       // cout<<filaFan[j].size();
+                       for (int h = 0; h < veces; h++){
+                            if (filaFan[j].size() != maximo_fila){
+                                for (int i = 0; i < cantFans; i++){
+                                Fanatico fan;
+                                filaFan[j].push(fan);
+                                cout<<"Llegaron "<< cantFans <<" a la fila numero "<< j<<endl;
+                            // this_thread::sleep_for(tiempo_llegar * chrono::seconds(1));
+                        }
+                        cout<<"La fila "<< j<< " esta lleno, hay "<< filaFan[j].size()<< " fanaticos."<< endl;
+                       }
+                        
+                    }
+                    }
+
+                vector<Graderia> graderias(cantGraderias);
+                for (int l = 0; l < cantGraderias; l++){
+                    for (int k = 0; k < cantFila; k++){
+                        if (graderias[l].obtenerSize()==maxGraderias){
+                            break;
+                        }
+                            while (!filaFan[k].empty()){
+                            Fanatico fan = filaFan[k].front();
+                            if (guarda.revisarEntrada(fan)){
+                                graderias[l].agregar(fan);
+                        }
+                    }
+                }
             }
-                cout<<"Llegaron "<< cantFans <<" a la cola. ";
-                this_thread::sleep_for(tiempo_llegar * chrono::seconds(1));
-            }
+                
+                duracionSim--;
+                this_thread::sleep_for(chrono::seconds(1));
             
+            }
         }
 };
 
