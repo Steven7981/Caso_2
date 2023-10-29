96f179646559e4019c37d328a7a12dec5efe1a72
Steven7981 <stevenfengwu79@gmail.com>
2023-10-29 02:45:39 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,15 +8,17 @@ using namespace std;
 
 int main(void){
     Threads thread; 
+    thread.iniciarSim();
 
     // Crear los hilos para las funciones
-    std::thread hiloLlegarFans(&Threads::llegarFans, &thread);
+  /*/  std::thread hiloLlegarFans(&Threads::llegarFans, &thread);
     std::thread hiloVerificarEnt(&Threads::verificarEnt, &thread);
     std::thread hiloAtenderCom(&Threads::atenderCom, &thread);
 
     // Iniciar los hilos
     hiloLlegarFans.join();
     hiloVerificarEnt.join();
-    hiloAtenderCom.join();
+    hiloAtenderCom.join();*/
+
 
 }
