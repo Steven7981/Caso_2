71a5ff877813d57fca5a82899d8137f5eea9b899
Steven7981 <stevenfengwu79@gmail.com>
2023-10-29 10:28:37 -0600

    Update cantante.h

--- cantante.h
+++ cantante.h
@@ -19,20 +19,8 @@ class Cantante{
         Cantante(){
 
         }
-        bool cantar(){
-            ifstream file("caso.json");
-            json data;
-            file >> data;
-            std::vector<int> canciones = data["canciones"];
-
-            for (auto cancion : canciones){
-                int duracion = cancion;
-                cout<<"Cantando una cancion con una duracion de "<< duracion;
-                this_thread::sleep_for(chrono::seconds(duracion));
-                canciones.erase(canciones.begin());
-            }
-            cout<<"Terminó de cantar todas las canciones. "<< endl;
-            return true;
+        void cantar(){
+            
             }
         
 
